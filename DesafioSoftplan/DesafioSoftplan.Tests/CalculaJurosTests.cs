86cc5899d866c0d70805671966dd2622699e3307
Gustavo Ferreira <gustavofvieira@live.com>
2021-03-10 20:27:36 -0300

    Finalizado xUnit

---
+++ DesafioSoftplan/DesafioSoftplan.Tests/CalculaJurosTests.cs
@@ -0,0 +1,27 @@
+﻿using DesafioSoftplan.Domain.ViewModels;
+using Xunit;
+
+namespace DesafioSoftplan.Tests
+{
+    public class CalculaJurosTests
+    {
+        [Fact]
+        public void SetaParametros_RelizaCalculo_RetornaCalculoVM()
+        {
+            //Arrange
+            var calculoVM = new CalculaJurosVM()
+            {
+                ValorInical = 100,
+                Meses = 5
+            };
+
+            //Act
+            var jurosTotal = calculoVM.CalculaJuros(calculoVM);
+
+            //Assert
+            Assert.NotNull(jurosTotal);
+            Assert.IsType<double>(jurosTotal);
+            Assert.Equal(105.1, jurosTotal);
+        }
+    }
+}
